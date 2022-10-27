6c3eb3b977e0fc08412093aec405113cf80ad614
mgravemeyer <m.gravemeyer@icloud.com>
2022-10-27 13:52:01 -0400

    Hello World!

---
+++ routes/index.tsx
@@ -0,0 +1,15 @@
+import { Head } from "$fresh/runtime.ts";
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Maximilian Gravemeyer</title>
+      </Head>
+      <div class="">
+      </div>
+    </>
+  );
+};
+
+export default Index;
